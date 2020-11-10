9529b7379332afbd6d2cde453473973b93c2ecbc
Dimitri M <ibmiti@gmail.com>
2020-11-10 06:21:06 -0500

    sending selection sort alg

---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+const numbers = [99,44,6,2,1,5,64,63,87,283,4,0];
+
+function selectionSort(array){
+    const length = array.length;
+    for (i = 0; i < length; i++){
+        // set current index as minimum
+        let min = i;
+        let temp = array[i];
+        for(j = i+1; j < length; j++){
+            if (array[j] < array[min]){
+                // update minimum if current is lower than what we had previously
+                min = j;
+            }
+        }
+        array[i] = array[min];
+        array[min] = temp;
+    }
+    return array;
+}
+
+selectionSort(numbers);
+console.log(numbers);
